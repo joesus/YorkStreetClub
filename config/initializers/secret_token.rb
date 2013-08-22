b62cc552a68e9d8a24680703d7b42edbe16df092
Joesus <joesusnick@gmail.com>
2013-08-22 17:21:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YorkStreetClub::Application.config.secret_key_base = 'f7aa2c62af5539a7e4f34376b29237d3d5ed6bec9b766ea9e3d6bf9a831d971473b24a8d46da94ce7d137e9ef2379c83669920cdf31cd849bb7a3f563c0edcff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YorkStreetClub::Application.config.session_store :cookie_store, key: '_YorkStreetClub_session'
