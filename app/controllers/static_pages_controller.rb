ec70d008986391d1c3f7f02ae7cb1a2cff8e9b08
Joesus <joesusnick@gmail.com>
2013-08-22 19:26:39 -0600

    Fixed Title Helper

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def schedule
+  end
+
+  def about
+  end
+
+  def rules
+  end
+
+  def forsponsors
+  end
+
+  def forservice
+  end
+  
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def full_title(page_title)
+	base_title = "1311 York Street Club"
+	if page_title.empty?
+	  base_title
+	else 
+	  "#{page_title}"
+	end
+  end
 end
